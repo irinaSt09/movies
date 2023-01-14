771c538136d61b2f0e6701c748e4f67db8099e4f
Viktor Varbanov <varbanov@uber.com>
2023-01-14 21:53:54 +0200

    CORS v2

--- moviesBE/src/main/java/com/movies/spring/Beans.java
+++ moviesBE/src/main/java/com/movies/spring/Beans.java
@@ -3,6 +3,9 @@ package com.movies.spring;
 import info.movito.themoviedbapi.TmdbApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Beans {
@@ -10,5 +13,18 @@ public class Beans {
     @Bean
     public TmdbApi tmdbApi() {
         return new TmdbApi("ec5715c3f3247a05815345ae1ad60fe3");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/graphql/**")
+                        .allowedOrigins(CorsConfiguration.ALL)
+                        .allowedHeaders(CorsConfiguration.ALL)
+                        .allowedMethods(CorsConfiguration.ALL);
+            }
+        };
     }
 }
