a14d4f0939c90bf27259b85d13554714c3772395
YuliChavdarov <jachavdaro@uni-sofia.bg>
2023-01-14 21:28:18 +0200

    Rewrite jquery to react, add graphQL

--- movies-fe/src/App.js
+++ movies-fe/src/App.js
@@ -1,373 +1,206 @@
 import { useState } from "react";
+import "./App.css";
+import { gql, useQuery } from '@apollo/client';
+
+
 
 export default function App() {
 
-	const movie = {
-		release_date: "01.01.2023",
-		revenue: 1500,
-		runtime: "2h",
-		status: "asdf",
-		title: "movie"
+	// 	const GET_POPULAR_MOVIES = gql`
+	// 	query a {
+	// 		popularMovies{
+	// 		   id,
+	// 		title,
+	// 		production_countries {
+	// 		  iso_3166_1
+	// 		  name
+	// 		}
+	// 		}
+	// 	}
+	// `;
+
+	// const { loading, error, data } = useQuery(GET_POPULAR_MOVIES);
+
+	// if (loading) return <p>Loading...</p>;
+	// if (error) return <p>Error : {error.message}</p>;
+	// if(data) {
+	// 	console.log(data);
+	// 	return <p>DATAAAA LOOOK AT THE CONSOLE</p>;
+	// }
+
+
+	const genreTopRated = {
+		id: 1,
+		name: "Top Rated"
 	};
 
-	const [genres, setGenres] = useState([movie]);
+	const genrePopularity = {
+		id: 2,
+		name: "Popularity"
+	};
 
-	setGenres([movie]);
+	const genreSomethingElse = {
+		id: 3,
+		name: "Something Else"
+	};
 
-	const sortMovies = (field) => {
+	// ...get more from graphql query
 
+
+	// const [genres, setGenres] = useState([movie]);
+	const [currentSelectedGenreId, setCurrentSelectedGenreId] = useState(3);
+	const [searchText, setSearchText] = useState("");
+
+	const genres = [genreTopRated, genrePopularity, genreSomethingElse];
+	// setGenres([movie]);
+
+	const handleGenreFilterChange = (event, genreId) => {
+		event.preventDefault();
+		setCurrentSelectedGenreId(genreId);
+
+		//setGenres[filtered]; //get filtered from graphql or filter on client
 	}
 
-	return (
-		<>
-		<div>AAAAAAA</div>
-		{
-			genres.map(movie => {
-				<div>
-					{movie.release_date}
-				</div>
-			})
-		}
-		</>
-	);
+	const handleSearchTextChange = (event) => {
+		event.preventDefault();
+		console.log(event.target.value);
+		setSearchText(event.target.value);
+		//filter movies by name containing searchText
+	}
+
+	const handleMovieClick = (event, movieId) => {
+		event.preventDefault();
+		console.log("Show movie info test");
+		//show movieInfo on screen as a single card
+	}
+
+	const pussInBootsMovie = {
+		id: 315162,
+		genres: null,
+		title: "Puss in Boots: The Last Wish",
+		production_countries: null,
+		rating: "6.9"
+	};
+
+	const movies = [pussInBootsMovie]; //move to state, get movies with graphql query
+
+
+
+	//////////////////// Jquery shit
+	//////////////////// Jquery shit
+	//////////////////// Jquery shit
+
+	var next = 1;
+	var nextTV = 1;
+
+	var posterPaths = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
+	var backgroundPaths = "http://image.tmdb.org/t/p/w1280";
+	var url = "https://api.themoviedb.org/3/discover/movie?";
+	var key = "&api_key=6b4357c41d9c606e4d7ebe2f4a8850ea";
+	var urlTV = "https://api.themoviedb.org/3/discover/tv?";
+	var moreTVinfo =
+		"https://api.themoviedb.org/3/tv/  +tvshow id+  ?&api_key=6b4357c41d9c606e4d7ebe2f4a8850ea";
+	var movieCast = "https://api.themoviedb.org/3/movie/";
+	var actorInfo = "https://api.themoviedb.org/3/discover/movie?&with_cast=";
+	var imdbLink = "http://www.imdb.com/title/";
+	var date = new Date();
+
+	//////////////////// Jquery shit
+	//////////////////// Jquery shit
+	//////////////////// Jquery shit
+
+
+
+
+
+
+
+
 
 	return (
 		<>
-			<div class="sidebar-container">
-				<a
-					class="logo-link"
-					href="#"
-					onclick="sortMovies('popularity')"
-				>
-					<img
-						src="https://i.imgur.com/AYldSBG.png"
-						class="logo-image"
-					/>
-				</a>
-				<h2 class="title-genre">Discover</h2>
-				<a
-					class="category-link current"
-					href="#"
-					onclick="sortMovies('popularity')"
-				>
-					<div class="genre">Popular</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('rating')"
-				>
-					<div class="genre">Top Rated</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('grossing')"
-				>
-					<div class="genre">Grossing</div>
-				</a>
-				<h2 class="title-genre">Genres</h2>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('action')"
-				>
-					<div class="genre">Action</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('adventure')"
-				>
-					<div class="genre">Adventure</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('animation')"
-				>
-					<div class="genre">Animation</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('comedy')"
-				>
-					<div class="genre">Comedy</div>
-				</a>
-				<a class="category-link" href="#" onclick="sortMovies('crime')">
-					<div class="genre">Crime</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('documentary')"
-				>
-					<div class="genre">Documentary</div>
-				</a>
-				<a class="category-link" href="#" onclick="sortMovies('drama')">
-					<div class="genre">Drama</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('family')"
-				>
-					<div class="genre">Family</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('fantasy')"
-				>
-					<div class="genre">Fantasy</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('history')"
-				>
-					<div class="genre">History</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('horror')"
-				>
-					<div class="genre">Horror</div>
-				</a>
-				<a class="category-link" href="#" onclick="sortMovies('music')">
-					<div class="genre">Music</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('mystery')"
-				>
-					<div class="genre">Mystery</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('romance')"
-				>
-					<div class="genre">Romance</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('science fiction')"
-				>
-					<div class="genre">Science Fiction</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('tv movie')"
-				>
-					<div class="genre">TV Movie</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('thriller')"
-				>
-					<div class="genre">Thriller</div>
-				</a>
-				<a class="category-link" href="#" onclick="sortMovies('war')">
-					<div class="genre">War</div>
-				</a>
-				<a
-					class="category-link"
-					href="#"
-					onclick="sortMovies('western')"
-				>
-					<div class="genre">Western</div>
-				</a>
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					href="#"
-					onclick="window.open('https://ko-fi.com/vincenzopiromalli')"
-					class="coffee"
-				>
-					<img
-						src="https://i.imgur.com/WP4kgsA.png"
-						alt="Buy me a coffee"
-					/>
-					<span style="margin-left: 5px;">Buy me a coffee</span>
-				</a>
+			<i title="Go to top" onclick="topFunction()" id="myBtn" class="fa fa-arrow-up" aria-hidden="true"></i>
+			<div class="container main">
+				<div class="sidebar">
+					<div class="sidebar-container">
+						<a
+							class="logo-link"
+							href="#a"
+							onclick="sortMovies('popularity')"
+						>
+							<img
+								src="https://i.imgur.com/AYldSBG.png"
+								class="logo-image"
+							/>
+						</a>
+						<h2 class="title-genre">Discover</h2>
+						{/* <a
+							class="category-link current"
+							href="#a"
+							onclick="sortMovies('popularity')"
+						>
+							<div class="genre">Popular</div>
+						</a> */}
+						{
+							genres.map(genre => {
+								return (
+									<a
+										className={`category-link ${genre.id == currentSelectedGenreId && "current"}`}
+										href="#a"
+										onClick={e => handleGenreFilterChange(e, genre.id)}
+									// onclick="sortMovies('rating')" //genreId
+									>
+										<div class="genre">{genre.name}</div>
+									</a>
+								);
+							})
+						}
+					</div>
+				</div>
+
+
+
+
+
+				<div class="search">
+					<form class="search-form"><button type="submit" class="search-button"><i class="fa fa-search"></i></button>
+						<input id="search" onChange={e => handleSearchTextChange(e)}
+							// onKeyPress="return checkSubmit(event)"
+							type="search"
+							placeholder="&nbsp;Search for a movie..." class="search-input" value={searchText} />
+					</form>
+				</div>
+
+
+
+
+				<div class="content">
+					<div class="inner-container">
+						<div class="titles">
+							<h1>Popular</h1>
+							<h2>movies</h2>
+						</div>
+						<div class="item-container">
+							{
+								movies.map((movie, index) => {
+									return <a className={`item link movies m${index}`} id={movie.id} onClick={e => handleMovieClick(e, movie.id)} href='#' >
+										<img src={movie.posterUrl || "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1"} class='image' />
+										<div class='item-inner'>
+											<h2 class='item-title'>{movie.title}</h2>
+											<span class='rating'>
+												<i class='fa fa-star' aria-hidden='true'>
+												</i>{movie.rating || "N/A"}
+											</span>
+										</div>
+									</a>
+								})
+							}
+						</div>
+						{/* <div class="load-more"><i class="fa fa-plus-circle more" aria-hidden="true"></i></div> //if there is pagination  */}
+					</div>
+				</div>
+
 			</div>
 		</>
 	);
 }
-//         <div className="App">
-//             AAAAAAAAAAAAAA
-//             <div class="sidebar-container">
-//                 <a
-//                     class="logo-link"
-//                     href="#"
-//                     onclick="sortMovies('popularity')"
-//                 >
-//                     <img
-//                         src="https://i.imgur.com/AYldSBG.png"
-//                         class="logo-image"
-//                     />
-//                 </a>
-//                 <h2 class="title-genre">Discover</h2>
-//                 <a
-//                     class="category-link current"
-//                     href="#"
-//                     onclick="sortMovies('popularity')"
-//                 >
-//                     <div class="genre">Popular</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('rating')"
-//                 >
-//                     <div class="genre">Top Rated</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('grossing')"
-//                 >
-//                     <div class="genre">Grossing</div>
-//                 </a>
-//                 <h2 class="title-genre">Genres</h2>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('action')"
-//                 >
-//                     <div class="genre">Action</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('adventure')"
-//                 >
-//                     <div class="genre">Adventure</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('animation')"
-//                 >
-//                     <div class="genre">Animation</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('comedy')"
-//                 >
-//                     <div class="genre">Comedy</div>
-//                 </a>
-//                 <a class="category-link" href="#" onclick="sortMovies('crime')">
-//                     <div class="genre">Crime</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('documentary')"
-//                 >
-//                     <div class="genre">Documentary</div>
-//                 </a>
-//                 <a class="category-link" href="#" onclick="sortMovies('drama')">
-//                     <div class="genre">Drama</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('family')"
-//                 >
-//                     <div class="genre">Family</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('fantasy')"
-//                 >
-//                     <div class="genre">Fantasy</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('history')"
-//                 >
-//                     <div class="genre">History</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('horror')"
-//                 >
-//                     <div class="genre">Horror</div>
-//                 </a>
-//                 <a class="category-link" href="#" onclick="sortMovies('music')">
-//                     <div class="genre">Music</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('mystery')"
-//                 >
-//                     <div class="genre">Mystery</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('romance')"
-//                 >
-//                     <div class="genre">Romance</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('science fiction')"
-//                 >
-//                     <div class="genre">Science Fiction</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('tv movie')"
-//                 >
-//                     <div class="genre">TV Movie</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('thriller')"
-//                 >
-//                     <div class="genre">Thriller</div>
-//                 </a>
-//                 <a class="category-link" href="#" onclick="sortMovies('war')">
-//                     <div class="genre">War</div>
-//                 </a>
-//                 <a
-//                     class="category-link"
-//                     href="#"
-//                     onclick="sortMovies('western')"
-//                 >
-//                     <div class="genre">Western</div>
-//                 </a>
-//                 <a
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                     href="#"
-//                     onclick="window.open('https://ko-fi.com/vincenzopiromalli')"
-//                     class="coffee"
-//                 >
-//                     <img
-//                         src="https://i.imgur.com/WP4kgsA.png"
-//                         alt="Buy me a coffee"
-//                     />
-//                     <span style="margin-left: 5px;">Buy me a coffee</span>
-//                 </a>
-//             </div>
-//         </div>
-//     );
-// }
--- movies-fe/src/index.js
+++ movies-fe/src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:8080/graphiql?path=/graphql',
+    cache: new InMemoryCache(),
+});
+
+// client
+//     .query({
+//         query: gql`
+//         query a {
+//             popularMovies{
+//                id,
+//             title,
+//             production_countries {
+//               iso_3166_1
+//               name
+//             }
+//             }
+//         }
+//     `,
+//     })
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <ApolloProvider client={client}> */}
+            <App />
+        {/* </ApolloProvider> */}
+    </React.StrictMode>
 );
